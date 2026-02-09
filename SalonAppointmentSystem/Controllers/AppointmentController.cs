00cb86e740387c12b8013e6befa69fc03615f162
anjalicodes511 <511anjalidas@gmail.com>
2026-02-09 18:00:35 +0530

    My Appointment Feature

--- SalonAppointmentSystem/Controllers/AppointmentController.cs
+++ SalonAppointmentSystem/Controllers/AppointmentController.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Dapper;
+using SalonAppointmentSystem.DAL;
+using SalonAppointmentSystem.Models;
+using SalonAppointmentSystem.Models.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -12,6 +16,34 @@ namespace SalonAppointmentSystem.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult AllAppointments()
+        {
+            var list = DapperORM.ReturnList<AppointmentVM>("GetAllAppointments");
+            return View(list);
+        }
+
+        public ActionResult Appointments()
+        {
+            return View();
+        }
+
+        public JsonResult MyAppointments()
+        {
+            if (Session["UserId"] == null)
+            {
+                Session.Abandon();
+                return Json(new
+                {
+                    success = false
+                }, JsonRequestBehavior.AllowGet);
+            }
+            int CustomerId = Convert.ToInt32(Session["UserId"]);
+            DynamicParameters dp = new DynamicParameters();
+            dp.Add("@CustomerId", CustomerId);
+            var list = DapperORM.ReturnList<AppointmentVM>("GetAppointementsByCustomerId", dp);
+            return Json(list, JsonRequestBehavior.AllowGet);
         }
     }
 }
---
+++ SalonAppointmentSystem/Models/ViewModels/AppointmentVM.cs
@@ -0,0 +1,23 @@
+﻿using SalonAppointmentSystem.Models.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SalonAppointmentSystem.Models.ViewModels
+{
+    public class AppointmentVM
+    {
+        public int AppointmentId { get; set; }
+        public DateTime AppointmentDate { get; set; }
+        public int CustomerId { get; set; }
+        public string CustomerName { get; set; }
+        public string StylistName {  get; set; }
+        public string ServiceName {  get; set; }
+
+        public TimeSpan StartTime {  get; set; }
+        public TimeSpan EndTime { get; set; }
+
+        public AppointmentStatus Status { get; set; }
+    }
+}
