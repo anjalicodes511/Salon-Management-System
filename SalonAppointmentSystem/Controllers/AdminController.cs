6d567a4c5e824628549a9b992de6b47ce5b0d418
anjalicodes511 <511anjalidas@gmail.com>
2026-02-13 18:00:23 +0530

    Updated Landing Page and added customer report list

---
+++ SalonAppointmentSystem/Controllers/AdminController.cs
@@ -0,0 +1,37 @@
+﻿using SalonAppointmentSystem.DAL;
+using SalonAppointmentSystem.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Services.Description;
+
+namespace SalonAppointmentSystem.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult CustomerDetails()
+        {
+            return View();
+        }
+
+        public JsonResult GetCustomerDetails()
+        {
+            try
+            {
+                var list = DapperORM.ReturnList<CustomerDetailsVM>("GetAllCustomers");
+                return Json(list, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                return Json(new
+                {
+                    success = false,
+                    message = ex.Message
+                });
+            }
+        }
+    }
+}
---
+++ SalonAppointmentSystem/Models/ViewModels/CustomerDetailsVM.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SalonAppointmentSystem.Models.ViewModels
+{
+    public class CustomerDetailsVM
+    {
+        public int CustomerId { get; set; }
+        public string FullName {  get; set; }
+        public string Phone {  get; set; }
+        public string Email { get; set; }
+        public string Gender {  get; set; }
+        public bool IsActive { get; set; }
+
+        public int TotalAppointments {  get; set; }
+    }
+}
